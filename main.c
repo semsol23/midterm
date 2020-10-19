2198e2286b074b6a4f02e36fbc4e60874de4f7be
semsol23 <70558356+semsol23@users.noreply.github.com>
2020-10-19 23:14:53 +0900

    6-4

--- main.c
+++ main.c
@@ -3,21 +3,54 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a, int b)
-{
-	int sum;
+int getInt()
+{	
+	int a;
 	
-	sum = a+b;
-	return sum;
+	printf("input an integer : ");
+	scanf("%d", &a);
+	
+	return a;
+}
+
+int factorial(int a)
+{
+	int i;
+	int fac=1;
+	
+	for (i=1; i<=a; i++)
+	{
+		fac = fac*i;
+	}
+	
+	return fac;
+}
+
+int combination(int n, int r)
+{
+	int a, b, c;
+	int com;
+	
+	a = factorial(n);
+	b = factorial(n-r);
+	c = factorial(r);
+	
+	com = a/b/c;
+	
+	return com;
 }
 
 int main(int argc, char *argv[]) {
 	
-	int a, b;
-	int sum;
+	int n, r;
+	int result;
 	
-	sum = sumTwo(2, 5);
-	printf("%d", sum);
+	n = getInt();
+	r = getInt();
+	
+	result = combination(n, r);
 
+	printf("combination is %d\n", result);
+	
 	return 0;
 }
